4272d20e5a7c4affd333cd17281ada1e6a1daa34
Nils Kleinert <n.kleinert@kleto.de>
2024-10-19 17:11:10 +0200

    Add Column attribute to generated class fields
    Included the Column attribute for each field in the generated class. This ensures proper mapping between class properties and database columns.

--- src/index.js
+++ src/index.js
@@ -51,6 +51,7 @@ program
             const className = table.charAt(0).toUpperCase() + table.slice(1).toLowerCase();
             const output = [`[Table("${table}")]\npublic class ${className} {`];
             fields.forEach((element) => {
+                output.push(`    [Column("${element.fieldName}")]`);
                 output.push(`    public ${element.fieldType} ${element.fieldName.charAt(0).toUpperCase() + element.fieldName.slice(1)} { get; set; }`);
             });
             output.push(`}`);
